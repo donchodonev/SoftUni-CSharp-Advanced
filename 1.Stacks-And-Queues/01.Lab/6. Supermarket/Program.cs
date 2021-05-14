1bd4b9dec159dfa94dc5ce98b7e8f3152edc11c6
donchodonev <dummyacc9292@gmail.com>
2021-05-14 11:52:47 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/01.Lab/6. Supermarket/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<string> people = new Queue<string>();
+
+            int counter = 0;
+
+            while (input != "End")
+            {
+
+                if (input == "Paid")
+                {
+                    for (int i = 0; i < counter; i++)
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                    counter = 0;
+                }
+                else
+                {
+                    people.Enqueue(input);
+                    counter++;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
