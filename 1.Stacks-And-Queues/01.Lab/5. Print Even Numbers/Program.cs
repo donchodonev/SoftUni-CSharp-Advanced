f7dc13ae9f657d472ed6fc5ef8eee60578c11a43
donchodonev <dummyacc9292@gmail.com>
2021-05-14 11:35:42 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/01.Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> evenNums = new Queue<int>(numArr);
+
+            while (evenNums.Count > 1)
+            {
+                if (evenNums.Peek() % 2 != 0)
+                {
+                    evenNums.Dequeue();
+                }
+                else
+                {
+                    Console.Write($"{evenNums.Dequeue()}, ");
+                }
+            }
+            Console.Write($"{evenNums.Dequeue()}");
+        }
+    }
+}
