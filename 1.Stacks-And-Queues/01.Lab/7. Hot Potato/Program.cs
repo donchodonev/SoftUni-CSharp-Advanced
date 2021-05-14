95bb9281e6defcb0810f23d63b1b26a1c35f82b3
donchodonev <dummyacc9292@gmail.com>
2021-05-14 12:37:07 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/01.Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> kids = new Queue<string>(Console.ReadLine().Split().ToArray());
+
+            int n = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i == n)
+                    {
+                        Console.WriteLine($"Removed {kids.Dequeue()}");
+                    }
+                    else
+                    {
+                        kids.Enqueue(kids.Dequeue());
+                    }
+                }
+            }
+            Console.WriteLine($"Last is {kids.Peek()}");
+        }
+    }
+}
