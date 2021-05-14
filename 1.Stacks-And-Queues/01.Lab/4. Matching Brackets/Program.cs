fd9547f802588fe7edaf3370083fdf5f59595f93
donchodonev <dummyacc9292@gmail.com>
2021-05-14 11:13:43 +0300

    added new exercise solution

---
+++ 1.Stacks-And-Queues/01.Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> index = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    index.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    Console.WriteLine(expression.Substring(index.Peek(),i - index.Pop() + 1));
+                }
+            }
+        }
+    }
+}
