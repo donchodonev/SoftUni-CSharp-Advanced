a9ae4156b95119ae7fe7fb3c7aa5eb5273938d63
donchodonev <dummyacc9292@gmail.com>
2021-05-14 10:42:56 +0300

    added another exercise solution relocated .docx tasks file

---
+++ 1.Stacks-And-Queues/01.Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            input = input.Reverse().ToArray();
+
+            Stack<string> numsAndOperators = new Stack<string>();
+
+            foreach (var item in input)
+            {
+                numsAndOperators.Push(item);
+            }
+
+            while (numsAndOperators.Count > 1)
+            {
+                int firstNum = int.Parse(numsAndOperators.Pop());
+                string operatorSign = numsAndOperators.Pop();
+                int secondNum = int.Parse(numsAndOperators.Pop());
+
+                if (operatorSign == "+")
+                {
+                    numsAndOperators.Push((firstNum + secondNum).ToString());
+                }
+                else
+                {
+                    numsAndOperators.Push((firstNum - secondNum).ToString());
+                }
+            }
+
+            Console.WriteLine(numsAndOperators.Peek());
+        }
+    }
+}
