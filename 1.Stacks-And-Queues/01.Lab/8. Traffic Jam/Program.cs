7076e5a17d4347ab8e74a621f091c846bb1efcad
donchodonev <dummyacc9292@gmail.com>
2021-05-14 12:57:53 +0300

    added final exercise solution

---
+++ 1.Stacks-And-Queues/01.Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+
+            string carOrColor = Console.ReadLine();
+
+            int carsPassed = 0;
+
+            while (carOrColor != "end")
+            {
+                if (carOrColor == "green")
+                {
+                    if (n > cars.Count)
+                    {
+                        n = cars.Count;
+                    }
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        carsPassed++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(carOrColor);
+                }
+                carOrColor = Console.ReadLine();
+            }
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+        }
+    }
+}
