03e156a64e855b31664b548d95ffa3ce586faa04
donchodonev <dummyacc9292@gmail.com>
2021-05-14 09:55:42 +0300

    added first exercise solution added .docx exercise tasks file

---
+++ 1.Stacks-And-Queues/01.Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            Stack<char> reversedInput = new Stack<char>();
+
+            foreach (var character in input)
+            {
+                reversedInput.Push(character);
+            }
+
+            while (reversedInput.Count > 0)
+            {
+                Console.Write(reversedInput.Pop());
+            }
+        }
+    }
+}
