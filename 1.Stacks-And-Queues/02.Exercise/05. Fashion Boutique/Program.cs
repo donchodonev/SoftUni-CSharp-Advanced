c18cefae3852ec15d70484c8b659d468de556090
donchodonev <dummyacc9292@gmail.com>
2021-05-15 10:14:17 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/02.Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+
+            int rackSize = int.Parse(Console.ReadLine());
+
+            int rackCount = 1;
+
+            int clothesSizeSum = 0;
+
+            while (clothes.Count > 0)
+            {
+                if (clothes.Peek() + clothesSizeSum <= rackSize)
+                {
+                    clothesSizeSum += clothes.Pop();
+                }
+                else
+                {
+                    rackCount++;
+                    clothesSizeSum = 0;
+                    clothesSizeSum += clothes.Pop();
+                }
+            }
+
+            Console.WriteLine(rackCount);
+        }
+    }
+}
