1f474d94d129b5d2f4c063f175f3c8cf6c417ac9
donchodonev <dummyacc9292@gmail.com>
2021-05-15 10:14:06 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/02.Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            Queue<int> ordersQuantity = new Queue<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            Console.WriteLine(ordersQuantity.Max());
+
+            while (ordersQuantity.Count > 0)
+            {
+                if (ordersQuantity.Peek() <= quantityOfFood)
+                {
+                    quantityOfFood -= ordersQuantity.Dequeue();
+
+                    if (ordersQuantity.Count == 0)
+                    {
+                        Console.WriteLine("Orders complete");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.Write("Orders left: ");
+                    while (ordersQuantity.Count > 1)
+                    {
+                        Console.Write($"{ordersQuantity.Dequeue()} ");
+                    }
+                    Console.Write($"{ordersQuantity.Dequeue()}");
+                }
+            }
+        }
+    }
+}
