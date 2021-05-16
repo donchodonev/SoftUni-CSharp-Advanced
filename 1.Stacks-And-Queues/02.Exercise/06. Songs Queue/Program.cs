82964b14007da37aab3a4d00212b6ab163333b91
donchodonev <dummyacc9292@gmail.com>
2021-05-16 15:39:53 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/02.Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+            string commandArgs = Console.ReadLine();
+
+            while (songs.Count > 0)
+            {
+                if (commandArgs[0] == 'P')
+                {
+                    songs.Dequeue();
+                }
+                else if (commandArgs[0] == 'S')
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+                else
+                {
+                    string songNameValue = commandArgs.Split("Add ",StringSplitOptions.RemoveEmptyEntries)[0];
+
+                    if (songs.Contains(songNameValue))
+                    {
+                        Console.WriteLine($"{songNameValue} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(songNameValue);
+                    }
+                }
+
+                commandArgs = Console.ReadLine();
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
