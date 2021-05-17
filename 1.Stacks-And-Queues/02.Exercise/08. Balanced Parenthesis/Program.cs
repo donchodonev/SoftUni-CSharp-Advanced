396b6a4f5359cd2595555fc75ecd911727a3fc50
donchodonev <dummyacc9292@gmail.com>
2021-05-17 10:55:46 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/02.Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> open = new Stack<char>();
+
+            bool isValid = true;
+
+            foreach (char bracket in input)
+            {
+                if (bracket == '(' || bracket == '[' || bracket == '{')
+                {
+                    open.Push(bracket);
+                }
+                else
+                {
+                    if (open.Count == 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    else if (open.Peek().ToString() + bracket.ToString() == "()")
+                    {
+                        open.Pop();
+                        continue;
+                    }
+                    else if (open.Peek().ToString() + bracket.ToString() == "[]")
+                    {
+                        open.Pop();
+                        continue;
+                    }
+                    else if (open.Peek().ToString() + bracket.ToString() == "{}")
+                    {
+                        open.Pop();
+                        continue;
+                    }
+
+                }
+            }
+
+            if (isValid && open.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
