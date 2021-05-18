80b4057c6c3a79474b015acd3bbf53504084958b
donchodonev <dummyacc9292@gmail.com>
2021-05-18 10:13:47 +0300

    added another exercise solution

---
+++ 1.Stacks-And-Queues/02.Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+
+            int sizeOfBarrel = int.Parse(Console.ReadLine());
+
+            Stack<int> bulletStack = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            Queue<int> locksQueue = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            int intelligence = int.Parse(Console.ReadLine());
+
+            int shootCount = 0;
+
+            int shotsFired = 0;
+
+            while (true)
+            {
+                if (bulletStack.Count == 0 || locksQueue.Count == 0)
+                {
+                    if (bulletStack.Count == 0 && locksQueue.Count > 0)
+                    {
+                        Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count()}");
+                        return;
+                    }
+                    if (locksQueue.Count == 0)
+                    {
+                        Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${intelligence - (shotsFired * bulletPrice)}");
+                        return;
+                    }
+                }
+
+                //start shooting
+
+                if (bulletStack.Pop() <= locksQueue.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locksQueue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                shootCount++;
+                shotsFired++;
+
+                if (shootCount == sizeOfBarrel && bulletStack.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                    shootCount = 0;
+                }
+            }
+        }
+    }
+}
