d62c64a8ea03e911d6129e554cc4daa211f95d58
donchodonev <dummyacc9292@gmail.com>
2021-05-19 11:16:35 +0300

    added another exercise solution

---
+++ 2.Multidimensional-Arrays/1.Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] inputValues = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = inputValues[col];
+                }
+            }
+
+            int primaryDiagonalSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (col == row)
+                    {
+                        primaryDiagonalSum += matrix[row, col];
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(primaryDiagonalSum);
+        }
+    }
+}
