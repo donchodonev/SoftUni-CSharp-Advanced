f45c0a4254b0c3e90f0508a45304a578585f6507
donchodonev <dummyacc9292@gmail.com>
2021-05-19 10:23:28 +0300

    added new multidimensional arrays exercise solutions folder added exercises' tasks .docx file added first exercise solution

---
+++ 2.Multidimensional-Arrays/1.Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int rowLength = size[0];
+            int colLength = size[1];
+
+            int[,] matrix = new int[rowLength,colLength ];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(rowLength);
+            Console.WriteLine(colLength);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
