3584cb8fbbc2d579ccbc0888ef5b2a71cd6ef27d
donchodonev <dummyacc9292@gmail.com>
2021-05-20 19:16:03 +0300

    commented on code

--- 2.Multidimensional-Arrays/2.Exercise/8. Bombs/Program.cs
+++ 2.Multidimensional-Arrays/2.Exercise/8. Bombs/Program.cs
@@ -8,15 +8,19 @@ namespace _8._Bombs
     {
         public static int[,] TriggerBomb (int [,] matrix, int row, int col)
         {
+            //assing matrix
             int[,] currentMatrix = matrix;
 
+            //get cells damage value
             int damage = matrix[row, col];
 
+            //skip bombing if bomb value is less than 0
             if (damage <= 0)
             {
                 return currentMatrix;
             }
 
+            //initialise tuple queue with coordinates surrounding bomb
             Queue<(int dmgRow, int dmgCol)> splashDamage = new Queue<(int, int)>();
 
             splashDamage.Enqueue((-1, 1));//up and one right
@@ -29,8 +33,10 @@ namespace _8._Bombs
             splashDamage.Enqueue((0, 1));//just right
             splashDamage.Enqueue((0, 0));//current number
 
+            //sepeate coordinates' count as coorinates' count will get dynamically reduce in the for-loop
             int variationsCount = splashDamage.Count;
 
+            //modify bomb index to go through it's surrounding elements
             for (int spread = 0; spread < variationsCount; spread++)
             {
                 try
@@ -41,6 +47,7 @@ namespace _8._Bombs
                     }
                     splashDamage.Dequeue();
                 }
+                // if index out of range - dequeue coordinates and continue
                 catch (Exception)
                 {
                     splashDamage.Dequeue();
@@ -53,12 +60,13 @@ namespace _8._Bombs
 
         static void Main(string[] args)
         {
+            //get matrix size
             int matrixSize = int.Parse(Console.ReadLine());
 
+            //initialise matrix
             int[,] matrix = new int[matrixSize, matrixSize];
 
             //seed matrix
-
             for (int row = 0; row < matrixSize; row++)
             {
                 int[] input = Console.ReadLine()
@@ -72,11 +80,13 @@ namespace _8._Bombs
                 }
             }
 
-            //get coordinates pair
+            //get coordinates pair from console
             string[] stringCoordinates = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
+            //initialise a tuple queue keeping the row and col value where the bomb will be at
             Queue<(int row, int col)> coordinates = new Queue<(int, int)>();
 
+            //seed tuple queue
             for (int i = 0; i < stringCoordinates.Length; i++)
             {
                 int row = int.Parse(stringCoordinates[i][0].ToString());
@@ -85,8 +95,11 @@ namespace _8._Bombs
                 coordinates.Enqueue((row, col));
             }
 
+            //while coordinates exist - keep bombing
             while (coordinates.Count > 0)
             {
+                //assign bombed matrix to current matrix
+                //NB! ()
                 matrix = TriggerBomb(matrix, coordinates.Peek().row, coordinates.Peek().col);
 
                 coordinates.Dequeue();
@@ -95,7 +108,7 @@ namespace _8._Bombs
             int aliveCells = 0;
             int aliveCellsValueSum = 0;
 
-
+            //populate aliveCells count and aliveCellsValueSum
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int k = 0; k < matrix.GetLength(1); k++)
@@ -108,9 +121,11 @@ namespace _8._Bombs
                 }
             }
 
+            //print count and sum
             Console.WriteLine($"Alive cells: {aliveCells}");
             Console.WriteLine($"Sum: {aliveCellsValueSum}");
 
+            //print matrix
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int k = 0; k < matrix.GetLength(1); k++)
