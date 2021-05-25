c3f06aa52636188be1b48f4432cfe398c95feadb
donchodonev <dummyacc9292@gmail.com>
2021-05-25 15:51:33 +0300

    added another exercise solution

---
+++ 3.Sets and Dictionaries/2.Exercise/04. Even Times/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+
+            if (n == 0)
+            {
+                return;
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (nums.ContainsKey(num))
+                {
+                    nums[num]++;
+                }
+                else
+                {
+                    nums.Add(num, 1);
+                }
+            }
+
+            foreach (var num in nums.Where(x => x.Value % 2 == 0))
+            {
+                Console.WriteLine(num.Key);
+            }
+        }
+    }
+}
