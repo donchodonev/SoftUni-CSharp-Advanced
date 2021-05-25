70dbc7f62bdc561d88a63710efd7213816501912
donchodonev <dummyacc9292@gmail.com>
2021-05-25 12:16:44 +0300

    added sets and dictionaries repo folder added lab exercise folder added first exercise task's solution added .docx lab exercises' tasks

---
+++ 3.Sets and Dictionaries/1.Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] values = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> numDict = new Dictionary<double, int>();
+
+            foreach (var num in values)
+            {
+                if (numDict.ContainsKey(num))
+                {
+                    numDict[num]++;
+                }
+                else
+                {
+                    numDict.Add(num, 1);
+                }
+            }
+
+            foreach (var kvp in numDict)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
