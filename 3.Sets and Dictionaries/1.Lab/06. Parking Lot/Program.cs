9d12956f7d4e7bf5b360b5cb41ede38b659f4b9c
donchodonev <dummyacc9292@gmail.com>
2021-05-25 13:31:57 +0300

    added another exercise solution

---
+++ 3.Sets and Dictionaries/1.Lab/06. Parking Lot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            HashSet<string> licensePlates = new HashSet<string>();
+
+            while (inputArgs.Length != 1)
+            {
+                string direction = inputArgs[0];
+                string licensePlate = inputArgs[1];
+
+                if (direction == "IN")
+                {
+                    licensePlates.Add(licensePlate);
+                }
+                else
+                {
+                    if (licensePlates.Contains(licensePlate))
+                    {
+                        licensePlates.Remove(licensePlate);
+                    }
+                }
+
+                inputArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (licensePlates.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var carLicensePlate in licensePlates)
+                {
+                    Console.WriteLine(carLicensePlate);
+                }
+            }
+        }
+    }
+}
