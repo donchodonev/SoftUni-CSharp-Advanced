b40aa32d98e9a0685d5919f6d1dfb9c5bd90a497
donchodonev <dummyacc9292@gmail.com>
2021-05-25 14:47:13 +0300

    added new repo folder with exercises for sets and dictionaries added .docx exercise tasks file added first exercise solution

---
+++ 3.Sets and Dictionaries/2.Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
