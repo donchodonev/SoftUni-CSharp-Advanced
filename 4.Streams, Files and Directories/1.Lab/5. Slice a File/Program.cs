cf45c791af8edc326d104706bca3dc300bc47905
donchodonev <dummyacc9292@gmail.com>
2021-05-26 17:58:11 +0300

    changed while cycle condition for greater clarity added comments for greater clarity

--- 4.Streams, Files and Directories/1.Lab/5. Slice a File/Program.cs
+++ 4.Streams, Files and Directories/1.Lab/5. Slice a File/Program.cs
@@ -13,7 +13,7 @@ namespace _5._Slice_a_File
             int parts = 4;
 
             List<string> files = new List<string>
-            { 
+            {
                 "Part-1.txt",
                 "Part-2.txt",
                 "Part-3.txt",
@@ -22,24 +22,35 @@ namespace _5._Slice_a_File
 
             FileStream streamReadFile = new FileStream(sourceFile, FileMode.Open);
 
-            using (streamReadFile)
+            using (streamReadFile) //we open the source file
             {
-                long pieceSize = (long)Math.Ceiling((double)streamReadFile.Length / parts);
+                long pieceSize = (long)Math.Ceiling((double)streamReadFile.Length / parts); //define how large a quarter is
 
                 for (int i = 0; i < parts; i++)
                 {
+                    //reset current piece's size as it's needed to track piece progress
+
                     long currentPieceSize = 0;
+
+                    //create a new file
 
                     FileStream streamCreateFile = new FileStream($"{destinationDir}{files[i]}", FileMode.Create);
 
                     using (streamCreateFile)
                     {
-                        byte[] buffer = new byte[4096];
+                        byte[] buffer = new byte[4096]; //create an instance of the buffer holding bytes
 
-                        while (streamReadFile.Read(buffer, 0, buffer.Length) == buffer.Length)
+                        //The read method reads buffer.Length amount of bytes and stores them in the buffer
+                        //The write method takes the bytes and writes those to the current file
+
+                        //basically an endless cycle
+                        while (true)
                         {
-                            currentPieceSize += buffer.Length;
-                            streamCreateFile.Write(buffer, 0, buffer.Length);
+                            streamReadFile.Read(buffer, 0, buffer.Length); //fill buffer
+                            currentPieceSize += buffer.Length;//increase current piece size
+                            streamCreateFile.Write(buffer, 0, buffer.Length);//take buffer info and send it to file
+
+                            //make sure cycle ends when part size condition is met
 
                             if (currentPieceSize >= pieceSize)
                             {
