b1396ad508f9774edd7bb78edaf7a53d27cbb727
donchodonev <dummyacc9292@gmail.com>
2021-05-26 15:18:15 +0300

    added another exercise solution

---
+++ 4.Streams, Files and Directories/1.Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _02._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("resources\\Input.txt");
+
+            using (reader)
+            {
+                int counter = 1;
+
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter("resources\\output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
