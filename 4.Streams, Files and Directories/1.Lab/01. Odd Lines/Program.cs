74783e68b78a3bc99c54d38e087b4fc199d67675
donchodonev <dummyacc9292@gmail.com>
2021-05-26 15:02:43 +0300

    added first exercise solution

---
+++ 4.Streams, Files and Directories/1.Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _01._Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("resources\\Input.txt");
+
+            using (reader)
+            {
+                int counter = 0;
+
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter("resources\\Output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
