ddfafb4d23d6c3030d9c0d212a4f784c03bb776d
donchodonev <dummyacc9292@gmail.com>
2021-05-26 16:53:18 +0300

    added another exercise solution

---
+++ 4.Streams, Files and Directories/1.Lab/04.Merge Files/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace _04.Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader firstFile = new StreamReader("resources\\FileOne.txt");
+            StreamReader secondFile = new StreamReader("resources\\FileTwo.txt");
+            StreamWriter thirdFile = new StreamWriter("resources\\output.txt");
+
+            string third = string.Empty;
+
+            using (firstFile)
+            {
+                using (secondFile)
+                {
+                    string first = firstFile.ReadLine();
+                    string second = secondFile.ReadLine();
+
+                    while (first != null || second != null)
+                    {
+                        third += first;
+                        third += "\n";
+                        third += second;
+                        third += "\n";
+
+                        first = firstFile.ReadLine();
+                        second = secondFile.ReadLine();
+                    }
+                }
+            }
+
+            using (thirdFile)
+            {
+                thirdFile.WriteLine(third);
+            }
+        }
+    }
+}
