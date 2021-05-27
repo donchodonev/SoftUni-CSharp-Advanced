d929abf969d3c90b828d5e88d798854b383eb513
donchodonev <dummyacc9292@gmail.com>
2021-05-27 09:37:14 +0300

    added first exercise solution (empty) added .docx exercise tasks file added Resources folder - with resources

---
+++ 4.Streams, Files and Directories/2.Exercise/01.Even Lines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
