ceb81d89f542f523d8b38de41980cd62270d03ef
donchodonev <dummyacc9292@gmail.com>
2021-05-27 09:31:58 +0300

    added final exercise solution

---
+++ 4.Streams, Files and Directories/1.Lab/6. Folder Size/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _6._Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] paths = Directory.GetFiles("TestFolder\\");
+
+            double sizeSum = 0;
+
+            foreach (var file in paths)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sizeSum += fileInfo.Length;
+            }
+
+            sizeSum = sizeSum / 1024 / 1024;
+
+            File.WriteAllText("output.txt", sizeSum.ToString());
+
+        }
+    }
+}
