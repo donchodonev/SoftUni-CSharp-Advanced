1ab54fcbaedf9bb0bc2cc4afd36479744e20ff5c
donchodonev <dummyacc9292@gmail.com>
2021-05-30 10:47:02 +0300

    added another exercise solution

---
+++ 5.Functional Programming/1.Lab/04. Add VAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] priceWithVatAdded = Console.ReadLine()
+                .Split(", ")
+                .Select(decimal.Parse)
+                .Select(num => num * 1.2m)
+                .ToArray();
+
+
+            foreach (var num in priceWithVatAdded)
+            {
+                Console.WriteLine($"{num:F2}");
+            }
+        }
+    }
+}
