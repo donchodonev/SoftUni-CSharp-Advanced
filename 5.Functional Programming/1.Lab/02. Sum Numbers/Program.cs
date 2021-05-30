c5889ca9dac8fd0c74d18286292d935f4ec34791
donchodonev <dummyacc9292@gmail.com>
2021-05-30 10:38:23 +0300

    added another exercise solution

---
+++ 5.Functional Programming/1.Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+
+            Console.WriteLine(intList.Count());
+            Console.WriteLine(intList.Sum());
+        }
+    }
+}
