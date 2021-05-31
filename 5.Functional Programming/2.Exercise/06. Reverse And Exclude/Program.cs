7ef4c6ab39faf2793cf8f813c6f46570d3dc0ada
donchodonev <dummyacc9292@gmail.com>
2021-05-31 11:31:40 +0300

    added another exercise solution

---
+++ 5.Functional Programming/2.Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static Func<List<int>, int, List<int>> GetDivisibleBy = (listOfInts, divisibleBy) =>
+        {
+            List<int> filteredNums = new List<int>();
+
+            for (int i = listOfInts.Count - 1; i >= 0; i--)
+            {
+                if (listOfInts[i] % divisibleBy != 0)
+                {
+                    filteredNums.Add(listOfInts[i]);
+                }
+            }
+
+            return filteredNums;
+        };
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+
+            numbers = GetDivisibleBy(numbers, int.Parse(Console.ReadLine()));
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
