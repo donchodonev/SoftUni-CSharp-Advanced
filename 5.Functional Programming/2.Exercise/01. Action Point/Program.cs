bf7a41d668761b3c51c5caf5f84e3817a54de6b1
donchodonev <dummyacc9292@gmail.com>
2021-05-31 09:32:51 +0300

    revised folders' and projects' names

---
+++ 5.Functional Programming/2.Exercise/01. Action Point/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Action_Point
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Action<string[]> print = words =>
+            {
+                foreach (var word in words)
+                {
+                    Console.WriteLine(word);
+                }
+            };
+
+            print(Console.ReadLine().Split());
+        }
+    }
+}
---
+++ 5.Functional Programming/2.Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printSir = words =>
+            {
+                foreach (var word in words)
+                {
+                    Console.WriteLine($"Sir {word}");
+                }
+            };
+
+            printSir(Console.ReadLine().Split());
+        }
+    }
+}
