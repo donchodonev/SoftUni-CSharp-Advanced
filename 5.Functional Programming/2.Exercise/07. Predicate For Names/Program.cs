e56dc1801abc3282362a76383b9a4b7975c3bbdb
donchodonev <dummyacc9292@gmail.com>
2021-05-31 11:31:57 +0300

    added another exercise solution

---
+++ 5.Functional Programming/2.Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static Action<List<string>, int> PrintName = (names, length) =>
+        {
+            foreach (var name in names)
+            {
+                if (name.Length <= length)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        };
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            PrintName(names, n);
+        }
+    }
+}
