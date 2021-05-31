d089776c626311c4518fefde051d560ba8a15cb1
donchodonev <dummyacc9292@gmail.com>
2021-05-31 09:40:39 +0300

    added another exercise solution

---
+++ 5.Functional Programming/2.Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> printSmallest = nums =>
+             {
+
+                 int smallestNum = int.MaxValue;
+
+                 foreach (var num in nums)
+                 {
+                     if (num < smallestNum)
+                     {
+                         smallestNum = num;
+                     }
+                 }
+                 return smallestNum;
+             };
+            Console.WriteLine(printSmallest(Console.ReadLine().Split().Select(int.Parse).ToArray()));
+        }
+    }
+}
