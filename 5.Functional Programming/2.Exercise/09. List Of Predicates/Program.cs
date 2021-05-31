9848674e10c8c09908d696bbcde7f5c6ac593483
donchodonev <dummyacc9292@gmail.com>
2021-05-31 13:26:43 +0300

    added another exercise solution

---
+++ 5.Functional Programming/2.Exercise/09. List Of Predicates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numbers = Enumerable.Range(1, int.Parse(Console.ReadLine())).ToArray();
+            
+            int[] dividers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, bool> predicate = (num, divider) =>
+            {
+                return num % divider == 0;
+            };
+
+            foreach (var number in numbers)
+            {
+                if (dividers.All(divider => predicate(number, divider)))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
