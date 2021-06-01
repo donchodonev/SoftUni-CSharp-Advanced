98ab5ee6f3177700ea7b9135d857818a42c642a5
donchodonev <dummyacc9292@gmail.com>
2021-06-01 16:18:42 +0300

    added final exercise solution

---
+++ 5.Functional Programming/2.Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumOfChars = int.Parse(Console.ReadLine());
+
+            Func<string, int, bool> isCharSumEqualTo = (name, sumOfChars) =>
+            {
+                int sum = 0;
+
+                foreach (var character in name)
+                {
+                    sum += (int)character;
+                }
+                return sum >= sumOfChars;
+            };
+
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(names.FirstOrDefault(name => isCharSumEqualTo(name, sumOfChars)));
+        }
+    }
+}
