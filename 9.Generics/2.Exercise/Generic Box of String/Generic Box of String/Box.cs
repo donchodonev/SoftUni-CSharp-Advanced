81bf05766acf746859b2844205dbc929d6a3b99f
donchodonev <dummyacc9292@gmail.com>
2021-06-07 16:54:21 +0300

    added first exercise solution added .docx exercise tasks file

---
+++ 9.Generics/2.Exercise/Generic Box of String/Generic Box of String/Box.cs
@@ -0,0 +1,16 @@
+﻿namespace Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ 9.Generics/2.Exercise/Generic Box of String/Generic Box of String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Generic_Box_of_String;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
