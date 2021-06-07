f5547ace89477421e9c32236d939b60e60da22d0
donchodonev <dummyacc9292@gmail.com>
2021-06-07 16:54:39 +0300

    added another exercise solution

---
+++ 9.Generics/2.Exercise/Generic Box of Int/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Generic_Box_of_String;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine()));
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
