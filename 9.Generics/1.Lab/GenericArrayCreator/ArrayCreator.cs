31ee10326130d115b22fe9d4434a32e626f9a094
donchodonev <dummyacc9292@gmail.com>
2021-06-07 16:09:34 +0300

    added another exercise solution

---
+++ 9.Generics/1.Lab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] customArray = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                customArray[i] = item;
+            }
+            return customArray;
+        }
+    }
+}
