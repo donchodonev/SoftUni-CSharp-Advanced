890ad2ee50163619b6a66aab8a8d2da5e7c65cb3
donchodonev <dummyacc9292@gmail.com>
2021-06-07 17:36:29 +0300

    added another exercise solution

---
+++ 9.Generics/2.Exercise/Generic Swap Method String/Generic Swap Method String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Generic_Box_of_String;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<string>> listOfBoxes = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                listOfBoxes.Add(new Box<string>(Console.ReadLine()));
+            }
+
+            int [] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Box<string>.Swap(listOfBoxes, indexes[0], indexes[1]);
+
+            listOfBoxes.ForEach(box => Console.WriteLine(box));
+        }
+    }
+}
