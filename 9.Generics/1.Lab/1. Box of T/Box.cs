f72296c0c3bad56b0d79eae9a5fbcd16c680e571
donchodonev <dummyacc9292@gmail.com>
2021-06-07 15:55:41 +0300

    added first exercise solution from Generics lab added .docx exercises' tasks file

---
+++ 9.Generics/1.Lab/1. Box of T/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Stack<T> Elements { get; set; } = new Stack<T>();
+        public int Count { get => Elements.Count; }
+        public void Add(T element)
+        {
+            Elements.Push(element);
+        }
+        public T Remove()
+        {
+            return Elements.Pop();
+        }
+    }
+}
