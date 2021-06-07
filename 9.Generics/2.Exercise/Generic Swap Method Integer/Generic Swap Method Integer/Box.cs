88a7ed3cc9565d6f58dda874622037f96a30c579
donchodonev <dummyacc9292@gmail.com>
2021-06-07 17:41:00 +0300

    added another exercise solution

---
+++ 9.Generics/2.Exercise/Generic Swap Method Integer/Generic Swap Method Integer/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+        public static void Swap(List<Box<T>> listOfBox, int indexOne, int indexTwo)
+        {
+            Box<T> firstElement = listOfBox[indexOne];
+            Box<T> secondElement = listOfBox[indexTwo];
+
+            listOfBox[indexOne] = secondElement;
+            listOfBox[indexTwo] = firstElement;
+        }
+    }
+}
