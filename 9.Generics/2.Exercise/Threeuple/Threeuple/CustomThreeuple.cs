e9bbc4927f12ece2643f921eee46dd07c0f09eaa
donchodonev <dummyacc9292@gmail.com>
2021-06-09 11:19:18 +0300

    added another exercise solution

---
+++ 9.Generics/2.Exercise/Threeuple/Threeuple/CustomThreeuple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class CustomThreeuple<T1,T2,T3>
+    {
+        public T1 item1 { get; set; }
+        public T2 item2 { get; set; }
+        public T3 item3 { get; set; }
+    }
+}
---
+++ 9.Generics/2.Exercise/Threeuple/Threeuple/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            CustomThreeuple<string, string,string> nameAndAddress = new CustomThreeuple<string, string,string>();
+
+            nameAndAddress.item1 = $"{firstInput[0]} {firstInput[1]}";
+            nameAndAddress.item2 = $"{firstInput[2]}";
+
+            if (firstInput.Length > 4)
+            {
+                nameAndAddress.item3 = $"{firstInput[3]} {firstInput[4]} ";
+            }
+            else
+            {
+                nameAndAddress.item3 = $"{firstInput[3]}";
+            }
+
+            string[] secondInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            CustomThreeuple<string, int, bool> nameAndLitersOfBeer = new CustomThreeuple<string, int, bool>();
+
+            nameAndLitersOfBeer.item1 = secondInput[0];
+            nameAndLitersOfBeer.item2 = int.Parse(secondInput[1]);
+
+            if (secondInput[2].ToLower() == "not")
+            {
+                nameAndLitersOfBeer.item3 = false;
+            }
+            else
+            {
+                nameAndLitersOfBeer.item3 = true;
+            }
+
+            string[] thirdInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            CustomThreeuple<string, double, string> nameBalanceAndBank = new CustomThreeuple<string, double,string>();
+
+            nameBalanceAndBank.item1 = thirdInput[0];
+            nameBalanceAndBank.item2 = double.Parse(thirdInput[1]);
+            nameBalanceAndBank.item3 = thirdInput[2];
+
+            Console.WriteLine($"{nameAndAddress.item1} -> {nameAndAddress.item2} -> {nameAndAddress.item3}");
+            Console.WriteLine($"{nameAndLitersOfBeer.item1} -> {nameAndLitersOfBeer.item2} -> {nameAndLitersOfBeer.item3}");
+            Console.WriteLine($"{nameBalanceAndBank.item1} -> {nameBalanceAndBank.item2} -> {nameBalanceAndBank.item3}");
+        }
+    }
+}
