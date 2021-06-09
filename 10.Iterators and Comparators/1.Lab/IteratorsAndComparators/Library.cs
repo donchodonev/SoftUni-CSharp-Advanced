beb7b55212d2442f8f1693344b525db0bdec98f2
donchodonev <dummyacc9292@gmail.com>
2021-06-09 15:59:24 +0300

    added first part of exercise solution

---
+++ 10.Iterators and Comparators/1.Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        public Library(params Book[] books)
+        {
+            Books = new List<Book>(books);
+        }
+
+        public List<Book> Books { get; private set; }
+    }
+}
