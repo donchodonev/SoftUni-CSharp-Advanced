895512e326314c7e1c8ea0f21f4d54b8ab938ee6
donchodonev <dummyacc9292@gmail.com>
2021-06-10 10:31:09 +0300

    added first exercise solution

--- 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators/ListyIterator.cs
+++ 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators/ListyIterator.cs
@@ -4,7 +4,53 @@ using System.Text;
 
 namespace IteratorsAndComparators
 {
-    public class ListyIterator
+    public class ListyIterator<T>
     {
+        private int index;
+        public ListyIterator(params T[] colItems)
+        {
+            Create(colItems);
+        }
+        public List<T> Collection { get; set; }
+
+        public bool HasNext()
+        {
+            int nextIndex = index + 1;
+
+            if (nextIndex >= Collection.Count)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Create(params T[] items)
+        {
+            index = 0;
+            Collection = new List<T>(items);
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine($"{Collection[index]}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Operation!");
+                Environment.Exit(0);
+            }
+        }
     }
 }
--- 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators/Program.cs
+++ 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators/Program.cs
@@ -4,9 +4,39 @@ namespace IteratorsAndComparators
 {
     public class Program
     {
-       public static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string[] commandArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
+            string[] elements = new string[commandArgs.Length - 1];
+
+            for (int i = 1; i < elements.Length + 1; i++)
+            {
+                elements[i - 1] = commandArgs[i];
+            }
+
+            ListyIterator<string> listy = new ListyIterator<string>(elements);
+
+            string nextCommand = Console.ReadLine();
+
+            while (!nextCommand.ToLower().Contains("end"))
+            {
+                if (nextCommand.Contains("Move"))
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (nextCommand.Contains("Print"))
+                {
+                    listy.Print();
+                }
+                else if (nextCommand.Contains("HasNext"))
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+
+                nextCommand = Console.ReadLine();
+            }
         }
     }
 }
