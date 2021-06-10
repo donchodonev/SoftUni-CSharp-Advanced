cb288e7f494f92261c0f303f3d9f1da2e1043cb5
donchodonev <dummyacc9292@gmail.com>
2021-06-10 15:16:18 +0300

    added another exercise solution

--- 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators2/Person.cs
+++ 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators2/Person.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace IteratorsAndComparators
 {
-    public class Person
+    public class Person : IComparable<Person>
     {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
 
+        public string Name{ get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (Name.CompareTo(other.Name) != 0)
+            {
+                return Name.CompareTo(other.Name);
+            }
+            if (Age.CompareTo(other.Age) != 0)
+            {
+                return Age.CompareTo(other.Age);
+            }
+
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name.GetHashCode() + Age.GetHashCode());
+        }
     }
 }
--- 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators2/Program.cs
+++ 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IteratorsAndComparators
 {
@@ -6,7 +7,26 @@ namespace IteratorsAndComparators
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<Person> hashSet = new HashSet<Person>();
+
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                hashSet.Add(new Person(name, age));
+                sortedSet.Add(new Person(name, age));
+            }
+
+            Console.WriteLine(hashSet.Count);
+            Console.WriteLine(sortedSet.Count);
         }
     }
 }
