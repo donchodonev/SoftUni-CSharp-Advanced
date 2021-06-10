b2732e240b46400a702a01582a5c820bea83eee4
donchodonev <dummyacc9292@gmail.com>
2021-06-10 12:38:43 +0300

    added new exercise solution Added Lake.cs class file Implemented IEnumerable for Lake.cs

---
+++ 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators/Lake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Lake :IEnumerable<int>
+    {
+        public Lake(int[] stones)
+        {
+            Stones = stones;
+        }
+
+        public int[] Stones { get; set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Stones.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return Stones[i];
+                }
+            }
+            for (int i = Stones.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return Stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators/Program.cs
+++ 10.Iterators and Comparators/2.Exercise/IteratorsAndComparators/IteratorsAndComparators/Program.cs
@@ -7,38 +7,15 @@ namespace IteratorsAndComparators
     {
         public static void Main(string[] args)
         {
-            string command = Console.ReadLine();
+            int[] nums = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
-            Stack<int> myStack = new Stack<int>();
 
-            while (!command.Contains("END"))
-            {
-                if (command.Contains("Push"))
-                {
-                    int[] intsToPush = command
-                        .Substring(4)
-                        .Split(", ")
-                        .Select(int.Parse)
-                        .ToArray();
+            Lake myLake = new Lake(nums);
 
-                    myStack.Push(intsToPush);
-                }
-                else
-                {
-                    myStack.Pop();
-                }
-
-                command = Console.ReadLine();
-            }
-
-            foreach (var element in myStack)
-            {
-                Console.WriteLine(element);
-            }
-            foreach (var element in myStack)
-            {
-                Console.WriteLine(element);
-            }
+            Console.WriteLine(string.Join(", ", myLake));
         }
     }
 }
